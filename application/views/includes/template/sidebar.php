14091a9619b42a4a850d50fed79a71966350161d
jxcho <62160165@go.buu.ac.th>
2022-03-10 21:10:03 +0700

    V_user_management and sidebar

--- application/views/includes/template/sidebar.php
+++ application/views/includes/template/sidebar.php
@@ -31,7 +31,7 @@
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link " href="../assests/template/argon-dashboard-master/pages/billing.html">
+          <a class="nav-link " href="../User_Management/show_user_management">
             <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
               <i class="ni ni-credit-card text-success text-sm opacity-10"></i>
             </div>
