9315c4a737c240c468502d6f765bee3c609ee6a7
62160102 <62160102@go.buu.ac.th>
2022-03-10 20:32:40 +0700

    report

---
+++ application/views/consent/v_dashboard.php
@@ -0,0 +1,22 @@
+<!--
+    /*
+    * v_dashboard
+    * dashboard detail
+    * @input -
+    * @output -
+    * @author Phatchara Khongthandee Thitima Popila and Ponprapai Atsawanurak
+    * @Create date : 2565-01-25
+    */
+-->
+//css
+<style>
+
+</style>
+<div class="card">
+    <div class="card-header">
+
+        <head>
+            <h1>Report (ผลการโหวต)</h1>
+        </head>
+    </div>
+</div>
