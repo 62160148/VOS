de914d541e1687c3b9e34be2d247d6f6aaff562f
Chakrit B <62160148@go.buu.ac.th>
2022-03-14 14:17:02 +0700

    edit sidebar

--- application/views/includes/template/sidebar.php
+++ application/views/includes/template/sidebar.php
@@ -3,9 +3,9 @@
   <aside class="sidenav bg-white navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4 " id="sidenav-main">
     <div class="sidenav-header">
       <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
-      <a class="navbar-brand m-0" href=" https://demos.creative-tim.com/argon-dashboard/pages/dashboard.html " target="_blank">
-        <img src="assests/template/argon-dashboard-master/assets/img/logo-ct-dark.png" class="navbar-brand-img h-100" alt="main_logo">
-        <span class="ms-1 font-weight-bold">Argon Dashboard 2</span>
+      <a class="navbar-brand m-0" href="" target="_blank">
+        <img src="../assests/template/argon-dashboard-master/assets/img/Logo_Team_6_v2.png" class="navbar-brand-img h-100">
+        <span class="ms-1 font-weight-bold">Vote Online System</span>
       </a>
     </div>
     <hr class="horizontal dark mt-0">
