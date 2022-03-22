41bcd871ab1173f0f062469ad22d0a6b0ae6b646
Chakrit B <62160148@go.buu.ac.th>
2022-03-23 03:27:48 +0700

    finished login

--- application/models/M_vos_user_login.php
+++ application/models/M_vos_user_login.php
@@ -26,13 +26,12 @@ class M_vos_user_login extends vos_model
     * @author Chakrit Boonprasert
     * @Create Date 2564-03-19
     */
-    function check_login($User_login, $User_pass_login)
+    function check_login($user_name, $user_password)
     { //check User_login and Pass_login in database
         $sql = "SELECT *
-			FROM ttps_database.user_login AS ulog 
-			WHERE User_login='$User_login' 
-			AND User_pass_login = '$User_pass_login'
-			";
+			FROM vos_database.vos_user_login AS ulog 
+			WHERE ulog.user_name='$user_name' 
+			AND ulog.user_password = '$user_password'";
         $query = $this->db->query($sql);
         return $query;
     } //end check_login
@@ -89,7 +88,7 @@ class M_vos_user_login extends vos_model
         INNER JOIN vos_database.vos_person AS per
         ON ulog.user_per_id = per.per_id
         WHERE ulog.user_role = ?";
-        $query = $this->db->query($sql,array($this->user_role));
+        $query = $this->db->query($sql, array($this->user_role));
         return $query;
     } //end get_user_list
 
@@ -100,9 +99,5 @@ class M_vos_user_login extends vos_model
         WHERE user_name = ?";
         $query = $this->db->query($sql, array($this->username));
         return $query;
-
     }
-
-
-    
 }//end class M_vos_user_login
