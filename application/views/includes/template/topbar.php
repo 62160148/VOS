06a265716caa77bd377e3b070fc9820659753494
Chakrit B <62160148@go.buu.ac.th>
2022-03-28 10:04:15 +0700

    add name and role in topbar

--- application/views/includes/template/topbar.php
+++ application/views/includes/template/topbar.php
@@ -7,7 +7,14 @@
         <div class="ms-md-auto pe-md-3 d-flex align-items-center">
         </div>
         <ul class="navbar-nav justify-content-end">
-          <li class="nav-item d-flex align-items-center">
+          <li class="nav-item d-flex align-items-center nav-link text-white font-weight-bold px-0">
+            Name : <?php echo $_SESSION['UsFullName'] . ' ' . $_SESSION['UsLastName']; ?>
+            <?php $role = $_SESSION['Usrole'];
+            if ($role == 1) { ?>
+              <span class="d-sm-inline d-none">&ensp;Role : Member |&ensp;</span>
+            <?php } else { ?>
+              <span class="d-sm-inline d-none">&ensp;Role : Admin |&ensp;</span>
+            <?php } ?>
             <a href="<?php echo site_url() . 'Login/logout' ?>" class="nav-link text-white font-weight-bold px-0">
               <i class="fas fa-sign-out-alt"></i>
               <span class="d-sm-inline d-none">Logout</span>
